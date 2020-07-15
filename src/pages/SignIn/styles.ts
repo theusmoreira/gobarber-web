b372e943853670f97b47a878c1b2777a56977a6d
Matheus Santos <matheussm301@gmail.com>
2020-07-14 23:32:18 -0300

    chore: Cria e estiliza componente button e input

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -27,40 +27,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      color: #f4ede8;
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      margin-top: 16px;
-      height: 56px;
-      background: #ff9000;
-      color: #312e38;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      width: 100%;
-      font-weight: 500;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
