b9a3e8bd7cc15b549d2e93c215c816e9c7696ad7
Matheus Santos <matheussm301@gmail.com>
2020-07-17 10:48:30 -0300

    chore: Muda nome de AuthContext para Auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
 import ToastContainer from './components/ToastContainer';
-import { AuthProvider } from './hooks/AuthContext';
+import { AuthProvider } from './hooks/Auth';
 
 const App: React.FC = () => (
   <>
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/Auth';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
