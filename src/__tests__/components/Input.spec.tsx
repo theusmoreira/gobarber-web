a5639716a21c322223faa420a2f8be12c9414aae
Matheus Santos <matheussm301@gmail.com>
2020-10-03 17:46:45 -0300

    test: Fiz bug in test for input

--- src/__tests__/components/Input.spec.tsx
+++ src/__tests__/components/Input.spec.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import Input from '../../components/Input';
+import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('@unform/core', () => {
   return {
@@ -26,17 +27,44 @@ describe('Input component', () => {
   });
 
   it('should render hightlight on input focus', async () => {
-    const { getByPlaceholderText, getByTestId } = render(
+    render(
       <Input name="email" placeholder="E-mail" />,
     );
 
-    const inputElement = getByPlaceholderText('E-mail');
-    const containerElement = getByTestId('input-container');
+    const inputElement = screen.getByPlaceholderText('E-mail');
+    const containerElement = screen.getByTestId('input-container');
 
     fireEvent.focus(inputElement);
 
     await waitFor(() => {
-      expect(containerElement).toHaveStyle('border-color: #ff9000;');
+      expect(containerElement).toHaveStyle({
+        borderColor: '#ff9000',
+      });
+    });
+
+    fireEvent.blur(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).not.toHaveStyle('border-color: #ff9000;');
+      expect(containerElement).not.toHaveStyle('color: #ff9000;');
+    });
+  });
+
+  it('should keep input border hightlight when input filled', async () => {
+    render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    const inputElement = screen.getByPlaceholderText('E-mail');
+    const containerElement = screen.getByTestId('input-container');
+
+    fireEvent.change(inputElement, {
+      target: { value: 'johndoe@example.com.br' }
+    })
+
+    fireEvent.blur(inputElement);
+
+    await waitFor(() => {
       expect(containerElement).toHaveStyle('color: #ff9000;');
     });
   });
